03db68dc3cfc418a1d03b9513825c38a3a0de40c
Amirul <amirulal73@gmail.com>
2023-04-16 18:21:42 +0800

    Create bootstrap view page and can view all memebers

--- simpleCRUD/app_CRUD/views.py
+++ simpleCRUD/app_CRUD/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Member
+
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    mem = Member.objects.all()
+    context = {
+        'mem':mem,
+    }
+    return render(request,'index.html',context=context)
--- simpleCRUD/simpleCRUD/urls.py
+++ simpleCRUD/simpleCRUD/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',include('app_CRUD.urls')),
+    path('',include('app_CRUD.urls')),
 ]
